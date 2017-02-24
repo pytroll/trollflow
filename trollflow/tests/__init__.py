0edc62938d02a89dcbc4a0e13d8454c676ae8baf
Panu Lahtinen <pnuu+git@iki.fi>
2017-02-24 13:05:02 +0200

    Make tests a package

---
+++ trollflow/tests/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017, Panu Lahtinen
+
+# Author(s):
+
+#   Panu Lahtinen <panu.lahtinen@fmi.fi>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The tests package.
+"""
+
+import unittest
+# import doctest
+from trollflow.tests import (test_utils, )
+
+
+def suite():
+    """The global test suite.
+    """
+    mysuite = unittest.TestSuite()
+    # Test the documentation strings
+    # mysuite.addTests(doctest.DocTestSuite(image))
+    # Use the unittests also
+    mysuite.addTests(test_utils.suite())
+
+    return mysuite
